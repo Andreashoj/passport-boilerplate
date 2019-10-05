adbb398145699dd4db7dcadfdd3f74be95db2a27
Andreashoj <32867811+Andreashoj@users.noreply.github.com>
2019-10-05 15:15:24 +0200

    Delete keys.js

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://Andreashoj:andreaz11@cluster0-klhmy.mongodb.net/test?retryWrites=true&w=majority',
-    cookieSecret: 'KASIOPDJMSAm,zkxmklmxzlkdmlzmdmMSMkdmks'
-}
